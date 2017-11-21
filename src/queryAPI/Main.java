45b3944b099d67e11f3c1b266661d777b925316e
NOXXA96 <mattia.nozza.96@gmail.com>
2017-11-21 09:59:02 +0100

    Start thread refactoring
    easy idea for divide items in lists

--- src/queryAPI/Main.java
+++ src/queryAPI/Main.java
@@ -3,6 +3,8 @@ package queryAPI;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -50,7 +52,7 @@ public class Main {
 			Integer.parseInt(s);
 			return true;
 		} catch (NumberFormatException ex) {
-			System.out.println("Perchè mi fai questo :(");
+			System.out.println("PerchÃ¨ mi fai questo :(");
 			return false;
 		}
 	}
@@ -62,6 +64,71 @@ public class Main {
 		System.out.println("####   \t     \\->[20 blocchi default]<-/\t\t####");
 		System.out.println("####################################################");
 
+	}
+	
+	public ArrayList<List<Integer>> dividiblocchi (int nBlocchi){
+		//per 8 thread;
+		int nextThread = 0;
+		List<Integer> t0 = new ArrayList<>();
+		List<Integer> t1 = new ArrayList<>();
+		List<Integer> t2 = new ArrayList<>();
+		List<Integer> t3 = new ArrayList<>();
+		List<Integer> t4 = new ArrayList<>();
+		List<Integer> t5 = new ArrayList<>();
+		List<Integer> t6 = new ArrayList<>();
+		List<Integer> t7 = new ArrayList<>();
+		ArrayList<List<Integer>> listaDiListe = new ArrayList<>();
+		
+		
+		for (int i=0; i<nBlocchi; i++) {
+			switch (nextThread) {
+			case 0:
+				nextThread = 1;
+				t0.add(i);
+				break;
+			case 1:
+				nextThread = 2;
+				t1.add(i);
+				break;
+			case 2:
+				nextThread = 3;
+				t2.add(i);
+				break;
+			case 3:
+				nextThread = 4;
+				t3.add(i);
+				break;
+			case 4:
+				nextThread = 5;
+				t4.add(i);
+				break;
+			case 5:
+				nextThread = 6;
+				t5.add(i);
+				break;
+			case 6:
+				nextThread = 7;
+				t6.add(i);
+				break;
+			case 7:
+				nextThread = 0;
+				t7.add(i);
+				break;
+			}	
+		}
+		
+		listaDiListe.add(t0);
+		listaDiListe.add(t1);
+		listaDiListe.add(t2);
+		listaDiListe.add(t3);
+		listaDiListe.add(t4);
+		listaDiListe.add(t5);
+		listaDiListe.add(t6);
+		listaDiListe.add(t7);
+		
+		return listaDiListe;
+		
+		
 	}
 
 }
